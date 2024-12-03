fc7e41ebf7ebca6ff515a4669cb5ee48e379234d
Netra1996 <hiremathdaksha35@gmail.com>
2024-12-03 20:31:59 +0530

    My Python Project

---
+++ app.py
@@ -0,0 +1,52 @@
+print('Netra')
+
+name = 'Netra'
+
+age = 20
+
+Is_Patient = True
+
+msg=f'{name} {age} {Is_Patient}'
+print(msg)
+
+
+Birth_Year=input('Enter Birh Year')
+age=2019 - int(Birth_Year)
+print(age)
+
+course="Python For Beginners"
+print(course[0:-1])
+
+print(course.lower())
+print(course.upper())
+print(len(course))
+print(course.find('P'))
+print('Python' in course)
+print(course.replace('P','J'))
+
+another=course[:]
+print(another)
+
+
+first = 'netra'
+
+last = 'Hiremath'
+
+print(first + '' + last)
+print('*' * 10)
+print(12 + 1)
+print(12-10)
+print(20%5)
+print(23//4)
+
+course='''
+Hi ,
+here is code for learning Python
+
+Thank you
+'''
+print(course)
+
+x=10
+x+=2
+print(x)
